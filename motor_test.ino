069153564a20675752c56ce4d20dd5e095a8af06
Blake Walker <144559898+blakewalker4@users.noreply.github.com>
2026-02-28 20:00:10 -0500

    Added MPU 6050 code
    untested. I know the reading of the MPU works but i still need to test the PID error correction and tune some variables. 
    Most credit goes to Deepseek.
    I think we should figure out how to set up ESPNOW or some other kind of communication so we can get serial info from the robot while its running easier.

--- motor_test.ino
+++ motor_test.ino
@@ -1,35 +1,49 @@
+#include <basicMPU6050.h>
+
+// Motor and PWM stuff
 /*
-* Intro to robotics lab project
-* Robot car motor control code
-*/ 
+ * IN1   IN2   | Motor Behavior
+ * ----------  | 
+ * HIGH  LOW   | Forward
+ * LOW   HIGH  | Backward
+ * HIGH  HIGH  | Brake (fast stop)
+ * LOW   LOW   | Coast (slow stop)
+ */
+// Note: Motor speed is a 8 bit value between 0 and 255
+//       but anything below about 160 will not make the motors move
+
 
 // left motors
 #define ENA 32 // PWM pin for left motor speed
-#define IN1 33 // Digital pin - left motor behavior 1
-#define IN2 25 // Digital pin - left motor behavior 2
+#define IN1 33 // Digital pin - direction
+#define IN2 25 // Digital pin - direction
 
 // right motors
 #define ENB 14 // PWM pin for right motor speed
-#define IN3 26 // Digital pin - right motor behavior 1
-#define IN4 27 // Digital pin - right motor behavior 2
-
-#define ON 21
-
-/*
- * IN1/3   IN2/4   | Motor Behavior
- * ----------------| 
- * HIGH    LOW     | Forward
- * LOW     HIGH    | Backward
- * HIGH    HIGH    | Brake (fast stop)
- * LOW     LOW     | Coast (slow stop)
- */
-
-// Note: Motor speed is a 8 bit value between 0 and 255
-//       but anything below about 160 will not make the motors move
+#define IN3 26 // Digital pin - direction
+#define IN4 27 // Digital pin - direction
 
 // PWM settings
 #define PWM_FREQ 5000      // 5 kHz is good for DC motors
 #define PWM_RESOLUTION 8   // 8-bit resolution (0-255)
+
+// Speed settings
+int base_speed = 170;  // (0-255)
+int max_adjustment = 50;  // Maximum speed change between motors
+
+
+// IMU stuff
+basicMPU6050<> imu;
+
+// PID constants for drift correction
+float Kp = 1.5;  // Proportional gain - adjust as neccessary
+float Ki = 0.05; // Integral gain - adjust as neccessary
+float Kd = 0.1;  // Derivative gain - adjust as neccessary
+
+float previous_error = 0;
+float integral = 0;
+unsigned long last_time = 0;
+
 
 void setup() {
   // Setup PWM for speed control
@@ -41,35 +55,78 @@ void setup() {
   pinMode(IN2, OUTPUT); 
   pinMode(IN3, OUTPUT); 
   pinMode(IN4, OUTPUT); 
-  
-  pinMode(ON, INPUT);
 
-  delay(100);
-  Serial.begin(9600);
+  // Setup MPU6050
+  imu.setup();
+  imu.setBias();  // Calibrate gyro when car is stationary
   
+  Serial.begin(115200);
+  last_time = micros();
 }
 
 void loop() {
-  bool on_state = digitalRead(ON);
-
   // set all motors to off by default
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, HIGH);
-
-  int speed = 170; // minimum 170
+  stop(100);
   
-  while(on_state){
-    speedSweep(speed); 
-    if(255 > speed+5)
-      speed += 5;
-    else
-      speed = 170;
-    on_state = digitalRead(ON);
-  }
+  // Calculate loop time for proper PID
+  unsigned long now = micros();
+  float dt = (now - last_time) / 1000000.0;  // Convert to seconds
+  last_time = now;
+  
+  // Update gyro readings
+  imu.updateBias();
+  
+  // Get gyro Z-axis (turning rate)
+  float turn_rate = imu.gz();  // degrees per second
+  
+  // We want turnRate to be 0 (going straight)
+  float error = 0 - turn_rate;
+  
+  // PID calculations
+  integral += error * dt;
+  float derivative = (error - previous_error) / dt;
+  
+  // Calculate correction
+  float correction = Kp * error + Ki * integral + Kd * derivative;
+  
+  // Constrain correction
+  correction = constrain(correction, -max_adjustment, max_adjustment);
+  
+  // Calculate motor speeds
+  int left_speed = base_speed - correction;   // If correction positive, left slower
+  int right_speed = base_speed + correction;  // If correction positive, right faster
+  
+  // Constrain speeds
+  left_speed = constrain(left_speed, 0, 255);
+  right_speed = constrain(right_speed, 0, 255);
+
+  // Set motor directions
+  // set left motors to forward
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  // set right motors to forward
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  
+  // Apply PWM speeds
+  ledcWrite(ENA, left_speed);
+  ledcWrite(ENB, right_speed);
+  
+  // Debug output
+  Serial.print("Turn Rate: ");
+  Serial.print(turn_rate);
+  Serial.print(" deg/s, Correction: ");
+  Serial.print(correction);
+  Serial.print(" L: ");
+  Serial.print(left_speed);
+  Serial.print(" R: ");
+  Serial.println(right_speed);
+  
+  previous_error = error;
+  delay(10);  // 100Hz control loop
   
 }
+
 
 void turnCCW(int speed, int time_dur){
   // set right motors to reverse
@@ -164,5 +221,3 @@ void speedSweep(int speed){
   goBackward(speed,300);
   stop(100);
 }
-
-
